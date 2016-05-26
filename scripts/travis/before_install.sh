14d8db0c777adf743b30591efe5de20c69623f8b
Daniel Kubec <niel@rtfm.cz>
2016-05-27 01:04:46 +0200

    Fixed travis path.

--- scripts/travis/before_install.sh
+++ scripts/travis/before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-./scripts/version.sh $TRAVIS_BRANCH
+./scripts/travis/version.sh $TRAVIS_BRANCH
 export VERSION="$BUILD_MAJOR"
 export PATCHLEVEL="$BUILD_MINOR"
 export SUBLEVEL="$BUILD_REVISION"

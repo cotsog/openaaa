2f32ade9d5f581b57ae2c1b4b4c30cd7f9aa909a
Daniel Kubec <niel@rtfm.cz>
2016-05-27 00:36:22 +0200

    Fixed Travis settings.

--- scripts/travis/before_install.sh
+++ scripts/travis/before_install.sh
@@ -18,13 +18,13 @@ if [ "$BUILD_TARGET" == "win64" ]; then
   export MINGW=/opt/mingw64 
   export PATH=$MINGW/bin:$PATH 
 fi
-if ["$TRAVIS_OS_NAME" == "linux"]; then 
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   sudo apt-get -qq update 
   sudo apt-get install flex bison gperf libncurses-dev pkg-config 
   sudo apt-get install binutils-mingw-w64-i686 gcc-mingw-w64-i686 
   sudo apt-get install binutils-mingw-w64 gcc-mingw-w64 
   fi
-if ["$TRAVIS_OS_NAME" == "osx"]; then
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
   unset CROSS_COMPILE 
   brew update 
   brew install flex bison gperftools 

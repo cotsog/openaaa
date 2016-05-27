48c2fc0be4a8d83c9dd63406a0066b7d65ae2bc8
Daniel Kubec <niel@rtfm.cz>
2016-05-26 21:54:44 -0400

    Fixed travis paths.

--- scripts/travis/before_install.sh
+++ scripts/travis/before_install.sh
@@ -10,7 +10,6 @@ num=`echo $TRAVIS_BRANCH | cut -d. -f2`
 if [ $num = *[[:digit:]]* ]; then export BUILD_MINOR=$num ; fi
 num=`echo $TRAVIS_BRANCH | cut -d. -f3`
 if [ $num = *[[:digit:]]* ]; then export BUILD_REVISION=$num ; fi
-echo "release: $BUILD_MAJOR $BUILD_MINOR $BUILD_REVISION"
 
 export BUILD_OS_RELEASE=$(uname -r)
 export BUILD_OS_NAME=$(uname -s)
@@ -19,12 +18,11 @@ export VERSION="$BUILD_MAJOR"
 export PATCHLEVEL="$BUILD_MINOR"
 export SUBLEVEL="$BUILD_REVISION"
 
-
 if [ "$BUILD_TARGET" == "win32" ]; then 
   unset CC 
   export CROSS_COMPILE="i686-w64-mingw32-"
   export MINGW=/opt/mingw64 
-  export PATH=$MINGW/bin:$PATH 
+  export PATH=$PATH:$MINGW/bin
   export BUILD_OS_NAME="win"
   export BUILD_OS_RELEASE="i686"
 fi 
@@ -32,7 +30,7 @@ if [ "$BUILD_TARGET" == "win64" ]; then
   unset CC 
   export CROSS_COMPILE="x86_64-w64-mingw32-"
   export MINGW=/opt/mingw64 
-  export PATH=$MINGW/bin:$PATH 
+  export PATH=$PATH:$MINGW/bin
   export BUILD_OS_NAME="win"
   export BUILD_OS_RELEASE="x86_64"
 fi
@@ -46,3 +44,4 @@ echo "build-version: $VERSION"
 echo "build-target: $BUILD_TARGET"
 echo "build-release: $BUILD_OS_RELEASE"
 echo "build-name: $BUILD_OS_NAME"
+echo "build-release: $BUILD_MAJOR $BUILD_MINOR $BUILD_REVISION"

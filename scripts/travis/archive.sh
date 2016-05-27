719eba3a8c4ce0143770df91ed00a2157b297fcf
Daniel Kubec <niel@rtfm.cz>
2016-05-27 05:01:39 +0200

    Add deploy condition.

--- scripts/travis/archive.sh
+++ scripts/travis/archive.sh
@@ -2,6 +2,6 @@
 #files:=$(find . -regex ".*\.\(dll\|so\|dylib\|exe\)")
 #files=$(find ./obj -perm -u=x -type f)
 files=$(find ./obj -type f -exec test -x {} \; -print)
-echo "archive-files: $files"
 echo "$files" | xargs tar -czvf $1
 echo "Deploying $1 to GitHub Releases"
+ls -la $1

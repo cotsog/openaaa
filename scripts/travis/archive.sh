8545d38d23ce62d2a56836ea7c4aed63fcab59ba
Daniel Kubec <niel@rtfm.cz>
2016-05-26 20:31:01 -0400

    Fixed find script for executables.

--- scripts/travis/archive.sh
+++ scripts/travis/archive.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #files:=$(find . -regex ".*\.\(dll\|so\|dylib\|exe\)")
-files=$(find ./obj -perm -111 -type f)
+files=$(find ./obj -perm -a=x -type f)
 echo "Deploying $1 to GitHub Releases"
 echo "$files" | xargs tar -czvf $1

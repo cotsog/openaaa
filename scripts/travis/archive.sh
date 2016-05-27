e0af990e5725ed00d2101bb36bcc2ec489a95793
Daniel Kubec <niel@rtfm.cz>
2016-05-26 21:55:37 -0400

    Fixed travis paths2.

--- scripts/travis/archive.sh
+++ scripts/travis/archive.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #files:=$(find . -regex ".*\.\(dll\|so\|dylib\|exe\)")
 #files=$(find ./obj -perm -u=x -type f)
-files=$(find ./obj/ -type f -exec test -x {} \; -print)
+files=$(find ./obj -type f -exec test -x {} \; -print)
 echo "Deploying $1 to GitHub Releases"
 echo "$files" | xargs tar -czvf $1

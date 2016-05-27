cb16cef4b002205278efefa190ef619d72c4fac1
Daniel Kubec <niel@rtfm.cz>
2016-05-26 20:51:38 -0400

    Improved deployment.

--- scripts/travis/before_install.sh
+++ scripts/travis/before_install.sh
@@ -12,22 +12,27 @@ num=`echo $TRAVIS_BRANCH | cut -d. -f3`
 if [ $num = *[[:digit:]]* ]; then export BUILD_REVISION=$num ; fi
 echo "release: $BUILD_MAJOR $BUILD_MINOR $BUILD_REVISION"
 
+export BUILD_OS_RELEASE=$(uname -r)
+export BUILD_OS_NAME=$(uname -s)
+
 export VERSION="$BUILD_MAJOR"
 export PATCHLEVEL="$BUILD_MINOR"
 export SUBLEVEL="$BUILD_REVISION"
-echo "build-version: $VERSION"
-echo "build-target: $BUILD_TARGET"
+
+
 if [ "$BUILD_TARGET" == "win32" ]; then 
   unset CC 
   export CROSS_COMPILE="i686-w64-mingw32-"
   export MINGW=/opt/mingw64 
   export PATH=$MINGW/bin:$PATH 
+  export BUILD_OS_NAME="win"
 fi 
 if [ "$BUILD_TARGET" == "win64" ]; then 
   unset CC 
   export CROSS_COMPILE="x86_64-w64-mingw32-"
   export MINGW=/opt/mingw64 
   export PATH=$MINGW/bin:$PATH 
+  export BUILD_OS_NAME="win"
 fi
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
   unset CROSS_COMPILE 
@@ -35,4 +40,5 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
   brew install flex bison gperftools 
 fi
 
-
+echo "build-version: $VERSION"
+echo "build-target: $BUILD_TARGET"
